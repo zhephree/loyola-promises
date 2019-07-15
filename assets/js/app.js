88dca6a8e5e69365094cdec5222a4c498e4ea499
Geoffrey Gauchet <ggauchet@lookfar.com>
2019-07-15 11:27:04 -0500

    get user's posts and albums

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+var loggedInUser = null;
 $("#login").click(function(){
 	fetch('https://jsonplaceholder.typicode.com/users?username=' + $('#username').val())
 		.then(function(response){
@@ -10,6 +11,32 @@ $("#login").click(function(){
 			$("#homepage").show();
 
 			$("#name").html(user.name);
+
+			loggedInUser = user;
+
+			return fetch('https://jsonplaceholder.typicode.com/posts?userId=' + user.id);
+		})
+		.then(function(response){
+			return response.json();
+		})
+		.then(function(posts){
+			posts.forEach(function(post){
+				var li = $('<li></li>');
+				li.text(post.title);
+				$("#posts").append(li);
+			})
+
+			return fetch('https://jsonplaceholder.typicode.com/albums?userId=' + loggedInUser.id)
+		})
+		.then(function(response){
+			return response.json();
+		})
+		.then(function(albums){
+			albums.forEach(function(album){
+				var li = $('<li></li>');
+				li.text(album.title);
+				$('#albums').append(li);
+			})
 		})
 		.catch(function(error){
 			alert(error);
